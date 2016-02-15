44501f2849cd58b26cb5db4fe7647f730b89346d
loopback <luan_kk12@hotmail.com>
2016-02-15 01:00:19 -0300

    Submissão inicial.

---
+++ app/src/main/java/chat/ufc/br/chatexample/Config.java
@@ -0,0 +1,9 @@
+package chat.ufc.br.chatexample;
+
+/**
+ * Created by Joe on 5/28/2014.
+ */
+public class Config {
+    // Google Project Number
+    static final String GOOGLE_PROJECT_ID = "519264447280";
+}
